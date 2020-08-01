04cbfeed9a9e9b2cd79963ec1186ad2ae0c902d0
bhargavannem <bhargav.annem@gmail.com>
2020-07-31 19:43:21 -0500

    Add firebase capabilities

--- Tutor.ml/ViewController.swift
+++ Tutor.ml/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UsernameOutlet: UITextField!
+    
+    @IBOutlet weak var PasswordOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnClicked(_ sender: Any) {
+        
+    }
+    
 }
 
